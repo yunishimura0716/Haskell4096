6f57c478d5f2cd5b540b4c9488850d5f82934be5
yunishimura0716 <yunishimura0716@gmail.com>
2023-02-10 22:38:45 -0800

    initial commit

---
+++ app/Data.hs
@@ -0,0 +1,29 @@
+module Data where
+
+import System.Random
+
+
+-- direction Left, Right, Up and Down
+data Direction = L | R | U | D
+  deriving (Eq, Show)
+
+-- Grid and relevant types
+type Position = (Int, Int)
+
+data Grid = Grid {
+  value :: Int,
+  position :: Position
+}
+
+-- Board
+type Board = [Grid]
+
+-- Game State
+data GameState = GameState {
+  board :: Board,
+  randomSeed :: StdGen
+}
+
+-- Game Result
+data GameResult = EndOfGame 
+                | ContinueGame GameState
