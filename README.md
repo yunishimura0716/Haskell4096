29189f896b6a8af18076f2ca561419211953a4d2
yunishimura0716 <yunishimura0716@gmail.com>
2023-02-20 22:41:22 -0800

    added comments on the code and instruction on README

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
 # Haskell4096
 
+# Setup Instruction
+1. Make sure your machine can run `stack` command
+    - If not, you can refer to [this link](https://docs.haskellstack.org/en/stable/install_and_upgrade/)
+2. set up the project by the following command. Download the compiler (if necessary): stack setup
+
+    ```stack setup```
+3. Build the project:
+
+    ```stack build```
+4. Run executable file
+
+    ```stack exec haskell4098-exe```
+
+# How to play
+1. You choose which type of board. We have
+    - `4 * 4`, `5 * 5` or `6 * 6`
+2. Once a game begin, you can play 2048 with direction keys
+
+3. If you end the game, just type `Ctrl-C` on the console
+
 # ToDo
 - fix a bug on board logic if anything
 - implement Bounce Effect (add new property to Grid, e.g. bounce)
 - be able to allow the users to choose the size of board (4 * 4, 5 * 5, 6 * 6)
   - can do for 4 * 4 and 5 * 5 now
+- scoring logic (if possible)
 - write a test
 - create a docs
--- app/Board.hs
+++ app/Board.hs
@@ -10,11 +10,13 @@ import Render
 import UIConstant
 
 
+-- Compute the board size with a given integer, which is the number of rows
 boardSize :: Int -> Float
 boardSize n = gridSize * factor + dividerSize * (factor+1)
   where
     factor = fromIntegral n
 
+-- Given number of rows, this retuns the Picture object for board
 boardFrame :: Int -> Picture
 boardFrame numSlots = pictures [fullboard, slots]
   where
@@ -32,6 +34,7 @@ boardFrame numSlots = pictures [fullboard, slots]
         y = fromIntegral $ div n numSlots
 
 
+-- Render function for Board type. This will call a render funciton for each grids
 instance Model Board where
   render b = pictures [(boardFrame n), grids]
     where
@@ -42,12 +45,12 @@ instance Model Board where
       flatBoard = flattenBoard b
       n = length b
 
--- flatten the board to list of grids
+-- Flatten the board to list of grids
 flattenBoard :: Board -> [Grid]
 flattenBoard board =
   foldl (\acc x -> acc ++ x) [] board
 
--- create a board with a number of rows/columns
+-- Create a board with a number of rows/columns
 createBoard :: Int -> [[Grid]]
 createBoard n =
   [ [grid | grid <- grids, getY grid == i] | i <- [0..(n-1)] ]
@@ -56,7 +59,8 @@ createBoard n =
       lastIndex = n * n - 1
       getY = snd . position
 
--- clear the positions on the board
+-- For the given board, it modifies the position values of each grids and
+-- returns a board with correction position. 
 clearBoard :: Board -> Board
 clearBoard board =
   [ [(Grid (getGridValue x y) (x,y)) | x <- [0..(n-1)]] | y <- [0..(n-1)] ]
@@ -65,7 +69,8 @@ clearBoard board =
       getGrid x y = (board !! y) !! x
       n = length board
 
--- rotate the board by direction to be left shifting 
+-- Rotate the board by direction to be left shifting 
+-- Left shifting is the case that the grids will shift to the left in the board (direction = L)
 toLeftShiftBoard :: Direction -> Board -> Board
 toLeftShiftBoard dirct board
   | dirct == L = board
@@ -73,7 +78,8 @@ toLeftShiftBoard dirct board
   | dirct == U = map reverse (transpose board)
   | dirct == D = transpose board 
 
--- rotate the board by direction to be orignal from left shifting 
+-- Rotate the board by direction to be orignal from left shifting 
+-- `toLeftShiftBoard` function should be called with the given board before we call this
 fromLeftShiftBoard :: Direction -> Board -> Board
 fromLeftShiftBoard dirct board
   | dirct == L = board
@@ -82,7 +88,8 @@ fromLeftShiftBoard dirct board
   | dirct == D = transpose board
 
 
--- remove blank grid from left
+-- Remove blank grid from the list of grids.
+-- x and y are position values for next unblank grid
 removeBlanks :: Int -> Int -> [Grid] -> [Grid]
 removeBlanks _ _ [] = []
 removeBlanks x y (hd:tr) =
@@ -91,14 +98,17 @@ removeBlanks x y (hd:tr) =
   else
     (Grid (value hd) (x, y)):(removeBlanks (x+1) y tr)
 
--- add blanks to the tail of the list
+-- Add blank grids to the tail of the list
+-- n: the number of blank grides it's going to insert
+-- x & y: position values for next blank grid
 addBlanks :: Int -> Int -> Int -> [Grid] -> [Grid]
 addBlanks 0 _ _ grids = grids
 addBlanks n _ y [] = [(Grid 0 (i, y)) | i <- [0..(n-1)]]
 addBlanks n x y grids =
   addBlanks (n-1) (x+1) y (grids ++ [Grid 0 (x, y)])
 
--- left shifting on the list without blanks
+-- Execute left shifting on the list of grids, and returns the result
+-- x & y: position values for next grid
 leftShift :: Int -> Int -> [Grid] -> [Grid]
 leftShift _ _ [] = []
 leftShift x y [grid] = [Grid (value grid) (x, y)]
@@ -109,7 +119,8 @@ leftShift x y (hd:hd2:tr)
       (grid, result) = combineGrids x y hd hd2
       
 
--- behave left shifting on the list of grids
+-- Behave left shifting on the row of board
+-- Delete the blank grids first, execute left shifting, and add blank grids to the end
 leftShiftGrids :: [Grid] -> [Grid]
 leftShiftGrids [grid] = [grid] 
 leftShiftGrids grids =
@@ -121,11 +132,12 @@ leftShiftGrids grids =
       gridsNoBlank = removeBlanks 0 y grids
       (_, y) = position (last grids)
       
--- behave left shifting on the board
+-- Behave left shifting on the board
+-- Apply `leftShiftGrids` function to the each row of board
 leftShiftBoard :: Board -> Board
 leftShiftBoard board = map leftShiftGrids board
 
--- behave shifting with direction on the board
+-- Behave shifting with direction on the board
 shiftBoard :: Direction -> Board -> Board
 shiftBoard dirct board = 
   clearBoard newBoard
